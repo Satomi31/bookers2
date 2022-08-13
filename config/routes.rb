a4c0638c0e23033c4fe65378ae6f204d411fb156
Satomi31 <108987382+Satomi31@users.noreply.github.com>
2022-08-13 07:18:31 +0000

    [Add] ログインページまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:index, :show, :edit, :update]
+
+  devise_for :users
   root to: "homes#top"
   get 'home/about' => 'homes#about', as: 'about'
-  devise_for :users
+
+  resources :books, only: [:index, :show, :edit, :update]
+resources :users, only: [:index, :show, :edit, :update]
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
