8038de5389ab50280b9dd20ab44a7a406ae1d426
Satomi31 <108987382+Satomi31@users.noreply.github.com>
2022-08-14 13:27:08 +0000

    [Add] bookのcreate機能まで

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'home/about' => 'homes#about', as: 'about'
 
-  resources :books, only: [:index, :show, :edit, :update]
-resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :show, :edit, :update, :create]
+  resources :users, only: [:index, :show, :edit, :update]
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
